d157b1c8dad87565940a2650002f766d149f66e7
Psilo <psilo@unterrainer.info>
2022-06-03 16:40:38 +0200

    amend

--- src/main/java/info/unterrainer/server/mqttclient/Information.java
+++ src/main/java/info/unterrainer/server/mqttclient/Information.java
@@ -2,6 +2,6 @@ package info.unterrainer.server.mqttclient;
 
 public class Information {
 	public static final String name = "Elite-Server-DTOs";
-	public static final String buildTime = "2022-06-03T14:35:55Z";
-	public static final String pomVersion = "0.0.2";
+	public static final String buildTime = "2022-06-03T14:39:34Z";
+	public static final String pomVersion = "0.0.3";
 }
