31f661e14b37f644b2f0fd7513e05cee3825470b
Gerald Unterrainer <gerald.unterrainer@cms-building.at>
2022-07-04 13:48:59 +0200

    update #minor and others

--- src/main/java/info/unterrainer/server/mqttclient/Information.java
+++
@@ -1,7 +0,0 @@
-package info.unterrainer.server.mqttclient;
-
-public class Information {
-	public static final String name = "Elite-Server-DTOs";
-	public static final String buildTime = "2022-06-04T16:54:19Z";
-	public static final String pomVersion = "0.0.9";
-}
